fc732a1ae2710b7420692638f36baa2291d96323
Nathan Martinez <natedzmtz@yahoo.com>
2023-01-21 19:03:09 -0700

    Updating deployment requirements

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/fm_base_apparel_coming_soon/",
   plugins: [svelte()],
 })
