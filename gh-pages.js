d4052bf5042241b450a751979aac8a97fa34b6a7
Nathan Martinez <natedzmtz@yahoo.com>
2023-01-21 18:54:42 -0700

    Added svelte deployment files

---
+++ gh-pages.js
@@ -0,0 +1,16 @@
+var ghpages = require('gh-pages')
+
+ghpages.publish(
+	'public',
+	{
+		branch: 'main',
+		repo: 'https://github.com/NathanMartinez/fm_base_apparel_coming_soon',
+		user: {
+			name: 'Nathan Martinez',
+			email: 'natedzmtz@yahoo.com',
+		},
+	},
+	() => {
+		console.log('Deploy Complete!')
+	}
+)
